776af7acb46c668a54af3291425087395775b14e
kondo <>
2026-03-06 23:31:32 +0900

    [wip]とりあえずそれっぽいコードは書いたけどコンパイル通るかは不明

--- keymap.c
+++ keymap.c
@@ -20,31 +20,41 @@
 #include QMK_KEYBOARD_H
 #include "rdmctmzt_common.h"
 
+#define MY_VLEFT LGUI(LCTL(KC_LEFT))
+#define MY_TASKV LGUI(KC_TAB)
+#define MY_VRIGHT LGUI(LCTL(KC_RIGHT))
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_tkl_ansi(
-        KC_TAB,  KC_Q,    KC_W,      KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
-        KC_CAPS, KC_A,    KC_S,      KC_D,     KC_F,       KC_G,      KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,      KC_C,     KC_V,       KC_B,      KC_N,     KC_M,    KC_COMM, KC_DOT,   KC_UP ,   KC_QUOT,
-        MO(2),   KC_LCTL, KC_GRV,    KC_LGUI,  KC_LALT,               KC_SPC,   KC_RGUI, KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_Q,    KC_W,    KC_E,      KC_R,     KC_T,       KC_TAB,       KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,
+        KC_A,    KC_S,    KC_D,      KC_F,     KC_G,       KC_LALT,      KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
+        KC_Z,    KC_X,    KC_C,      KC_V,     KC_B,       KC_LGUI,      KC_N,     KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_BSLS,
+        MO(4),   KC_LCTL, KC_LSFT,   MO(1),    KC_SPC,                   KC_ENT,   MO(2),   KC_LCTL, KC_INS,   KC_PSCR,  KC_PAUS
     ),
     [1] = LAYOUT_tkl_ansi(
-        KC_TAB,  KC_Q,    KC_W,      KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
-        KC_CAPS, KC_A,    KC_S,      KC_D,     KC_F,       KC_G,      KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,      KC_C,     KC_V,       KC_B,      KC_N,     KC_M,    KC_COMM, KC_DOT,   KC_UP ,   KC_QUOT,
-        MO(3),   KC_LCTL, KC_GRV,    KC_LALT,  KC_LGUI,               KC_SPC,   KC_RALT, KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
+        LCTL(KC_Q), LCTL(KC_W), LCTL(KC_E),LCTL(KC_R),LCTL(KC_T), KC_INS,   KC_APP, KC_F7, KC_F8, KC_F9, KC_F10, KC_ESC,
+        LCTL(KC_A), LCTL(KC_S), LCTL(KC_D), LCTL(KC_F),LCTL(KC_G),_______,   KC_PGUP, KC_F4, KC_F5, KC_F6, KC_F11, KC_F14,
+        LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C),LCTL(KC_V), LCTL(KC_B),_______,   KC_PGDN, KC_F1, KC_F2, KC_F3, KC_F12, KC_F13,
+        _______   , _______,    _______, _______, _______, LSFT(LCTL(KC_ENT)), _______,    _______,    _______, _______, _______ 
     ),
     [2] = LAYOUT_tkl_ansi(
-        KC_TAB,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     RM_NEXT,   TO(1),    TO(0),   KC_LBRC, KC_RBRC,  KC_BSLS,  RM_TOGG,
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
+        MY_VLEFT,  MY_TASKV,  MY_VRIGHT, _______,_______,_______,KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_MINS,  KC_EQL,
+        KC_F15,   KC_F16,   KC_F17,  KC_F18,   KC_F19,_______,   KC_BSPC,  KC_DEL, KC_HOME,  KC_END,  KC_GRV, KC_RSFT,
+        _______, _______, _______, _______, KC_TAB, _______, _______, _______, _______, _______, _______
+    ),
+    [3] = LAYOUT_tkl_ansi(
+        RESET,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, _______, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    ),
+    [4] = LAYOUT_tkl_ansi(
+        KC_TAB,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     RM_NEXT,   _______,  _______,   KC_LBRC, KC_RBRC,  KC_BSLS,  RM_TOGG,
         KC_ESC,  KC_1,    KC_2,      KC_3,     KC_4,       KC_5,      KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,
         KC_LSFT, KC_INS,  KC_DEL,    KC_HOME,  KC_END,     KC_PGUP,   KC_PGDN,  RM_SATD, RM_HUED, RM_HUEU,  RM_VALU , QK_BAT,
         KC_NO,   KC_DEL,  KC_GRV,    KC_LGUI,  KC_LALT,               EE_CLR,   RM_SATU, KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
-    ),
-    [3] = LAYOUT_tkl_ansi(
-        KC_TAB,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     RM_NEXT,   TO(1),    TO(0),   KC_LBRC, KC_RBRC,  KC_BSLS,  RM_TOGG,
-        KC_ESC,  KC_1,    KC_2,      KC_3,     KC_4,       KC_5,      KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,
-        KC_LSFT, KC_INS,  KC_DEL,    KC_HOME,  KC_END,     KC_PGUP,   KC_PGDN,  RM_SATD, RM_HUED, RM_HUEU,  RM_VALU , QK_BAT,
-        KC_NO,   KC_DEL,  KC_GRV,    KC_LALT,  QK_WLO,                EE_CLR,   RM_SATU, KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
     )
 };
 // clang-format on
